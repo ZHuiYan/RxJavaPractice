7b06bad5da43bd4bea3a638ac6084fd5ffda0b5e
zhaohuiyan <zhaohuiyan@wordemotion.com>
2017-06-15 16:46:30 +0800

    BackPressure RxJavaPractice

---
+++ app/src/main/java/practice/zhy/com/rxjavapractice/BackpressureActivity.java
@@ -0,0 +1,13 @@
+package practice.zhy.com.rxjavapractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BackpressureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backpressure);
+    }
+}
